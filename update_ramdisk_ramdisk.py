fe450747af9ab8b388b549f866e1b2dcf87c0c68
haolong.zhang <haolong.zhang@ck-telecom.com>
2018-03-28 19:50:43 +0800

    create a quick build ramdisk.img python file from this point, we may try bring up a own os run a Android os

---
+++ update_ramdisk_ramdisk.py
@@ -0,0 +1,34 @@
+import sys
+import os
+
+if len(sys.argv) != 2:
+    print("need a param, which means ramdisk root dir!")
+    exit()
+
+if not os.path.exists(sys.argv[1]):
+    print("Invalid root dir!")
+    exit()
+
+if not os.path.exists('./scripts/gen_initramfs_list.sh'):
+    print("can not find ./scripts/gen_initramfs_list.sh, pls make kernel firstly!")
+    exit()
+
+if not os.path.exists('./usr/gen_init_cpio'):
+    print("can not find ./usr/gen_init_cpio, pls make kernel firstly!")
+    exit()
+
+print("gen file list now")
+if os.path.exists("/tmp/rootfs_file"):
+    print("rm old file list!")
+    os.system("rm /tmp/rootfs_file")
+
+gen_exec = "./scripts/gen_initramfs_list.sh %s > /tmp/rootfs_file" % sys.argv[1]
+os.system(gen_exec)
+
+print("gen new rootfs.cpio")
+if not os.path.exists('/tmp/rootfs_file'):
+    print("can not find /tmp/rootfs_file, gen file list failed!")
+    exit()
+
+gen_rootfs_exec = "./usr/gen_init_cpio /tmp/rootfs_file > rootfs.cpio"
+os.system(gen_rootfs_exec)
